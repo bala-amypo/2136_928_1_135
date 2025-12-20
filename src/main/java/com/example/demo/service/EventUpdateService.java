bdea601e528a5e743e322905c323b141b53d5a5a
bala-amypo <bala@amypo.in>
2025-12-20 09:50:05 +0000

    User commit

--- src/main/java/com/example/demo/service/EventUpdateService.java
+++ src/main/java/com/example/demo/service/EventUpdateService.java
@@ -1,16 +1,10 @@
-package com.example.demo.service;
-
 import java.util.List;
 import java.util.Optional;
 import com.example.demo.entity.EventUpdate;
 
 public interface EventUpdateService {
-
     EventUpdate save(EventUpdate update);
-
     List<EventUpdate> getAll();
-
-    Optional<EventUpdate> getById(Integer id);
-
+    Optional<EventUpdate> getById(Integer id); // ⚠️ make sure it returns Optional
     void delete(Integer id);
 }
