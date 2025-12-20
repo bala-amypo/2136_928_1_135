74f0c291c835fb77749784cb8b4f468a2c31f20e
bala-amypo <bala@amypo.in>
2025-12-20 09:20:05 +0000

    User commit

---
+++ src/main/java/com/example/demo/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.example.demo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgument(
+            IllegalArgumentException ex) {
+
+        return new ResponseEntity<>(
+                ex.getMessage(),
+                HttpStatus.BAD_REQUEST
+        );
+    }
+}
