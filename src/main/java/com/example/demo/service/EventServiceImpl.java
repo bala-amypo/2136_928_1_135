bd04dde0f6c661391191c52433a97ef50162e183
bala-amypo <bala@amypo.in>
2025-12-20 08:38:05 +0000

    User commit

--- src/main/java/com/example/demo/service/EventServiceImpl.java
+++ src/main/java/com/example/demo/service/EventServiceImpl.java
@@ -1,14 +1,3 @@
-package com.example.demo.service;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.example.demo.entity.Event;
-import com.example.demo.exception.ResourceNotFoundException;
-import com.example.demo.repository.EventRepository;
-
 @Service
 public class EventServiceImpl implements EventService {
 
@@ -16,26 +5,7 @@ public class EventServiceImpl implements EventService {
     private EventRepository eventRepository;
 
     @Override
-    public Event save(Event event) {
-        return eventRepository.save(event);
-    }
-
-    @Override
-    public List<Event> getAll() {
-        return eventRepository.findAll();
-    }
-
-    @Override
-    public Event getById(Integer id) {
-        return eventRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Event not found"));
-    }
-
-    @Override
-    public void delete(Integer id) {
-        if (!eventRepository.existsById(id)) {
-            throw new ResourceNotFoundException("Event not found");
-        }
-        eventRepository.deleteById(id);
+    public Optional<Event> getById(Integer id) {
+        return eventRepository.findById(id);
     }
 }
