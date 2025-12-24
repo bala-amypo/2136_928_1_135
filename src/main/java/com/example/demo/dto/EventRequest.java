5048b4319aadaa94f878ceb09a8941f436131bfd
bala-amypo <bala@amypo.in>
2025-12-24 06:29:24 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/EventRequest.java
@@ -0,0 +1,55 @@
+package com.example.demo.dto;
+
+import java.time.LocalDateTime;
+
+public class EventRequest {
+
+    private String title;
+    private String description;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private boolean isActive;
+
+    public EventRequest() {}
+
+    // Getters & Setters
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public boolean getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+}
---
+++ src/main/java/com/example/demo/exceptions/OperationNotAllowedException.java
@@ -0,0 +1,12 @@
+package com.example.demo.exception;
+
+public class OperationNotAllowedException extends RuntimeException {
+
+    public OperationNotAllowedException(String message) {
+        super(message);
+    }
+
+    public OperationNotAllowedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
