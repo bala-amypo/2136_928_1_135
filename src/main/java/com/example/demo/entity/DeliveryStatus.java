94b46ff556c237908b9898a731b2fd2b1cecfa72
bala-amypo <bala@amypo.in>
2025-12-26 08:10:04 +0000

    User commit

--- src/main/java/com/example/demo/entity/DeliveryStatus.java
+++ src/main/java/com/example/demo/entity/DeliveryStatus.java
@@ -1,7 +1,8 @@
 package com.example.demo.entity;
 
 public enum DeliveryStatus {
+    PENDING,
     SENT,
-    DELIVERED,
-    FAILED
+    FAILED,
+    DELIVERED
 }
---
+++ src/main/java/com/example/demo/entity/EventCategory.java
@@ -0,0 +1,9 @@
+package com.example.demo.entity;
+
+public enum EventCategory {
+    WEATHER,
+    TRAFFIC,
+    PUBLIC_SAFETY,
+    ENTERTAINMENT,
+    OTHER
+}
